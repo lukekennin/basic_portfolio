a8cde3d9896699b2a13488279fa001e41072c450
Luke Kennin <lkmeq@me.com>
2018-09-06 16:02:30 -0500

    mysql

--- assets/liri_bot/keys.js
+++ assets/liri_bot/keys.js
@@ -1,4 +1,4 @@
 exports.spotifyKeys = {
-    id: 'a89436e2a03849ebb85874102096c30d',
-    secret: 'd460ae06ef49433abf62ad97d3cc2c43',
+    id: process.env.a89436e2a03849ebb85874102096c30d,
+    secret: process.env.d460ae06ef49433abf62ad97d3cc2c43,
 }
